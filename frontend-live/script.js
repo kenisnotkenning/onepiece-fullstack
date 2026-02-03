8c49c4ff94d09d08ebe5322d4653c5657014cead
Your Name <your@email.com>
2026-02-03 23:45:33 +0530

    Added mobile hamburger menu

--- frontend-live/script.js
+++ frontend-live/script.js
@@ -249,4 +249,15 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+// ===== MOBILE MENU TOGGLE =====
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", () => {
+  if (navMenu.style.display === "flex") {
+    navMenu.style.display = "none";
+  } else {
+    navMenu.style.display = "flex";
+  }
+});
 

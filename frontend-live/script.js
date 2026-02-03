3a6d7fda1d616cec759acd008bb4593464a74029
Your Name <your@email.com>
2026-02-04 00:11:19 +0530

    Fixed mobile menu error

--- frontend-live/script.js
+++ frontend-live/script.js
@@ -253,11 +253,13 @@ window.addEventListener("scroll", () => {
 const menuToggle = document.getElementById("menuToggle");
 const navMenu = document.getElementById("navMenu");
 
-menuToggle.addEventListener("click", () => {
-  if (navMenu.style.display === "flex") {
-    navMenu.style.display = "none";
-  } else {
-    navMenu.style.display = "flex";
-  }
-});
+if (menuToggle && navMenu) {
+  menuToggle.addEventListener("click", () => {
+    if (navMenu.style.display === "flex") {
+      navMenu.style.display = "none";
+    } else {
+      navMenu.style.display = "flex";
+    }
+  });
+}
 
